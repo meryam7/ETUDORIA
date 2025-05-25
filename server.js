92a89ace15e2ce8c1b0d6717ddfe125734dbe033
Boualy Maryem <110495011+meryam7@users.noreply.github.com>
2025-05-25 18:34:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,12 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet()); // Security headers
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
+app.use(cors({ 
+  origin: ["http://deploy-mern-1whq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true 
+}
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
